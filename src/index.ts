d64d7000e22736dba6bef2a4f09f3b964f94fcde
bitbucket-bartkoelewijnef2 <bartkoelewijn@ef2.nl>
2022-09-21 16:45:09 +0200

    export type

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import Button from './components/Button';
-import ThemeProvider from './components/ThemeProvider';
+import ThemeProvider, { PartialTheme } from './components/ThemeProvider';
 import Container from './components/Container';
 import Row from './components/Row';
 
+export type { PartialTheme };
 export { Button, ThemeProvider, Container, Row };
